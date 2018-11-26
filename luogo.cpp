0723af99392a52725e0287de3f607cae24851afc
Unknown <giacomo.planc@gmail.com>
2018-11-26 10:50:51 +0100

    aggiunta classe luogo

---
+++ luogo.cpp
@@ -0,0 +1,9 @@
+#include "luogo.h"
+
+Luogo::Luogo(string _provincia, string _com, string _via, int _nCivico, int _cap){
+	provincia=_provincia;
+	comune=_com;
+	via=_via;
+	nCivico=_ncivico;
+	cap=_cap;
+}
---
+++ luogo.h
@@ -0,0 +1,18 @@
+#ifndef __LUOGO_H__
+#define __LUOGO_H__
+
+#include <string>
+class Luogo{
+	private:
+	   String provincia;
+	   String comune;
+	   String via;
+	   int cap;
+	   int nCivico;
+	public:
+		Luogo(string _provincia, string _com, string _via, int _nCivico, int _cap);
+		
+};
+
+
+#endif
