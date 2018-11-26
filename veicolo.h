e10b4e1f0f8bb4afafc1a716a9f492da5efcbd00
Unknown <daniele.mattedi@studenti.unitn.it>
2018-11-26 10:00:17 +0100

    veicolo

---
+++ veicolo.h
@@ -0,0 +1,20 @@
+#ifndef __VEICOLO_H__
+#define __VEICOLO_H__
+
+class Veicolo{
+	private:
+		string targa;
+		list <Bollo> listaBollo;
+		list <Furto> listaFurto;
+		list <Assicurazione> listaAssicurazione;
+		list <Revisione> listaRevisione;
+		Automobile automobile;
+		Proprietario proprietario;
+		Data dataImmatricolazione;
+		Data dataAcquisto;
+	public:
+		
+};
+
+
+#endif
