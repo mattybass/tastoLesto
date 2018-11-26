5ac8856866c5372f4b932c83ae6b6731a7834667
Unknown <daniele.mattedi@studenti.unitn.it>
2018-11-26 10:16:32 +0100

    add furto

---
+++ furto.cpp
@@ -0,0 +1,7 @@
+#include "furto.h"
+
+Furto::Furto(int _g, int _m, int _a, string _note, string _luogo):dataDenuncia(_g,_m,_a){
+	luogoDenuncia=_luogo;
+	noteDenuncia=_note;
+}
+
---
+++ furto.h
@@ -0,0 +1,23 @@
+#ifndef __FURTO_H__
+#define __FURTO_H__
+#include <string>
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+#include "data.h"
+
+class Furto{
+	private:
+		Data dataDenuncia;
+		string luogoDenuncia;
+		noteDenuncia;
+	public:
+		Furto(int _g, int _m, int _a, string _note, string _luogo);
+
+		
+};
+
+
+
+
