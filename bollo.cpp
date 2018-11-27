9bab34797f37fa9774062905d970e29f602724d2
Unknown <daniele.mattedi@studenti.unitn.it>
2018-11-27 11:42:47 +0100

    sistemazione bollo

--- bollo.cpp
+++ bollo.cpp
@@ -5,39 +5,39 @@ Bollo::Bollo(int _kw, int _catEuro, int _g,int _m,int _a):dataScadenzaB(_g,_m%12
 	switch(_catEuro){
 		case 0:
 			if(_kw<=100)
-				costo=(float)(_kw*3);
+				costo=((float)_kw)*3;
 			else
-				costo=(float)(_kw*4.5);
+				costo=((float)_kw)*4.5;
 			break;				
 		case 1:
 			if(_kw<=100)
-				costo=(float)(2.9*_kw);
+				costo=2.9*((float)_kw);
 			else
-				costo=(float)_kw*4.35;
+				costo=((float)_kw)*4.35;
 			break;				
 		case 2:
 			if(_kw<=100)
-				costo=(float)_kw*2.8;
+				costo=((float)_kw)*2.8;
 			else
-				costo=(float)_kw*4.2;
+				costo=((float)_kw)*4.2;
 			break;
 		case 3:
 			if(_kw<=100)
-				costo=(float)_kw*2.7;
+				costo=((float)_kw)*2.7;
 			else
-				costo=(float)_kw*4.05;
+				costo=((float)_kw)*4.05;
 			break;
 		case 4:
 			if(_kw<=100)
-				costo=(float)_kw*2.7;
+				costo=((float)_kw)*2.7;
 			else
-				costo=(float)_kw*4.05;
+				costo=((float)_kw)*4.05;
 			break;
 		case 5:
 			if(_kw<=100)
-				costo=(float)_kw*2.7;
+				costo=((float)_kw)*2.7;
 			else
-				costo=(float)_kw*4.05;
+				costo=((float)_kw)*4.05;
 			break;
 		default:
 			break;														
