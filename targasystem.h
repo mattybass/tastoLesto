15f99a46c7fa19662427d19229c4c4f9ae1401a5
Unknown <riccardo.ricci-1@studenti.unitn.it>
2018-11-28 10:37:57 +0100

    Aggiunta classe targasystem.h

---
+++ targasystem.h
@@ -0,0 +1,3 @@
+//chechavviso --> dovrà richiamare tutti i check della classe veicolo per un certo veicolo e poi, tramite il getVettore stampare se ci sono avvisi oppure no, pensare
+//                se si può fare togliendo il vettore di booleani!
+
