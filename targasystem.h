4a2b8e50c528aee8544406e65e31117ba6f7b4ca
Unknown <riccardo.ricci-1@studenti.unitn.it>
2018-11-30 15:56:41 +0100

    Aggiunta funzione chechavviso GLOBALE

--- targasystem.h
+++ targasystem.h
@@ -46,6 +46,7 @@ class TargaSystem{
 		//Altri metodi
 		void disattivaDenuncia(string _valTarga,string _codFisc="",string _partIva="");
 		void checkAvviso(string _valTarga,string _codFisc="",string _partIva="")const;
+		void checkAvvisoGlobal(string _valTarga)const;
 		//Metodi appoggio usate esclusivamente da altri metodi
 		bool searchAuto(string _valTarga)const;
 		set<TipoVeicolo>::iterator searchTipoVeicolo(Tipo _tipo, string _marca, string _modello, int _cilindrata, int _kw, int _catEuro, int _euroNcap, int _nAirbag);
